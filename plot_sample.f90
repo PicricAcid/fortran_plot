fb61062f44b2be5e8cd5a1b080f22fe7c6fbb7ab
竹内賀徳 <>
2020-10-26 13:06:34 +0900

    add: plot_sample.f90

---
+++ plot_sample.f90
@@ -0,0 +1,29 @@
+program plot_sample
+  use plot
+  implicit none
+
+  integer, parameter:: n = 10
+  integer, parameter:: iunit = 10
+  double precision, parameter:: pi = 3.1415926535
+
+  integer:: i
+  double precision:: rand, y
+
+  open(iunit, file="./plot.dat", status="unknown")
+
+  do i=1, n
+    call random_number(rand)
+    rand = 2*rand - 1
+    y = sin(pi*rand)
+
+    write(iunit, *) rand, ", ", y
+  end do
+
+  close(iunit)
+
+  call plot_options(file_name="plot", title="sapmle", xlabel="x", ylabel="y")
+  call plot_graph(file_name="plot", dat_file_name="plot")
+  call plot_graph(file_name="plot", dat_file_name="plot", option="with lines")
+  call plot_open("plot")
+
+end program plot_sample
